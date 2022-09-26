ca5caf92b4da0aa2fe2a578e671935dc2f079b9f
EgorovGA <egorov.ga@minc.tech>
2022-09-26 17:03:28 +0300

    Two triangles, not transparent

--- homework01/homework01.cpp
+++ homework01/homework01.cpp
@@ -30,7 +30,7 @@ int main(int argc, char* argv[])
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	window = glfwCreateWindow( 512, 512, "Homework 1", NULL, NULL);
+	window = glfwCreateWindow( 1024, 1024, "Homework 1", NULL, NULL);
 	if( window == NULL ){
 		fprintf( stderr, "Failed to open GLFW window.\n" );
 		glfwTerminate();
@@ -51,18 +51,20 @@ int main(int argc, char* argv[])
 	glEnable(GL_BLEND);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-	// create memory for vertexes
-	GLuint VertexArrayID;
-	glGenVertexArrays(1, &VertexArrayID);
-	glBindVertexArray(VertexArrayID);
-
 	// Load shaders
 	char dir[1024];
    	getcwd( dir, sizeof( dir ) );
    	std::string cwd = dir;
-	GLuint programID = LoadShaders((cwd + "/../homework01/TransformVertexShader.vertexshader").c_str(),
-								   (cwd + "/../homework01/TextureFragmentShader.fragmentshader").c_str());
-	GLuint MatrixID = glGetUniformLocation(programID, "MVP");
+	GLuint programTriangle1ID = LoadShaders((cwd + "/../homework01/Triangle1.vertexshader").c_str(),
+								   (cwd + "/../homework01/Triangle1.fragmentshader").c_str());
+	GLuint MatrixTriangle1ID = glGetUniformLocation(programTriangle1ID, "MVP");
+	/*GLuint programTriangle2ID = LoadShaders((cwd + "/../homework01/Triangle2.vertexshader").c_str(),
+								   (cwd + "/../homework01/Triangle2.fragmentshader").c_str());
+	GLuint MatrixTriangle2ID = glGetUniformLocation(programTriangle1ID, "MVP");
+	GLuint programPyramidID = LoadShaders((cwd + "/../homework01/Pyramid.vertexshader").c_str(),
+								   (cwd + "/../homework01/Pyramid.fragmentshader").c_str());
+	GLuint MatrixPyramidID = glGetUniformLocation(programTriangle1ID, "MVP");
+	*/
 
 	// Angle of view: 45, ratio: 1:1, display distance: 0.1 <-> 100
 	glm::mat4 Projection = glm::perspective(glm::radians(45.0f), 1.0f / 1.0f, 0.1f, 100.0f);
@@ -83,122 +85,98 @@ int main(int argc, char* argv[])
 	//MVP = Model * Projection * View;
 	//MVP = Model * View * Projection;
 
-	// Vertices
-	static const GLfloat g_vertex_buffer_data[] = { 
-		-1.0f,-1.0f,-1.0f,
-		-1.0f,-1.0f, 1.0f,
-		-1.0f, 1.0f, 1.0f,
-		 1.0f, 1.0f,-1.0f,
-		-1.0f,-1.0f,-1.0f,
-		-1.0f, 1.0f,-1.0f,
-		 1.0f,-1.0f, 1.0f,
-		-1.0f,-1.0f,-1.0f,
-		 1.0f,-1.0f,-1.0f,
-		 1.0f, 1.0f,-1.0f,
-		 1.0f,-1.0f,-1.0f,
-		-1.0f,-1.0f,-1.0f,
-		-1.0f,-1.0f,-1.0f,
-		-1.0f, 1.0f, 1.0f,
-		-1.0f, 1.0f,-1.0f,
-		 1.0f,-1.0f, 1.0f,
-		-1.0f,-1.0f, 1.0f,
-		-1.0f,-1.0f,-1.0f,
-		-1.0f, 1.0f, 1.0f,
-		-1.0f,-1.0f, 1.0f,
-		 1.0f,-1.0f, 1.0f,
-		 1.0f, 1.0f, 1.0f,
-		 1.0f,-1.0f,-1.0f,
-		 1.0f, 1.0f,-1.0f,
-		 1.0f,-1.0f,-1.0f,
-		 1.0f, 1.0f, 1.0f,
-		 1.0f,-1.0f, 1.0f,
-		 1.0f, 1.0f, 1.0f,
-		 1.0f, 1.0f,-1.0f,
-		-1.0f, 1.0f,-1.0f,
-		 1.0f, 1.0f, 1.0f,
-		-1.0f, 1.0f,-1.0f,
-		-1.0f, 1.0f, 1.0f,
-		 1.0f, 1.0f, 1.0f,
-		-1.0f, 1.0f, 1.0f,
-		 1.0f,-1.0f, 1.0f
-	};
+	// create memory for vertices
+	GLuint VertexArrayID;
+	glGenVertexArrays(1, &VertexArrayID);
+	glBindVertexArray(VertexArrayID);
 
-	// Colors
-	static const GLfloat g_color_buffer_data[] = { 
+	// Create models
+	// Triangle1
+	static const GLfloat g_vertex_triangle1_buffer_data[] = { 
+		-1.0f,-1.0f,-1.0f,
+		-1.0f,-1.0f, 1.0f,
+		-1.0f, 1.0f, 1.0f,
+		-3.0f,-1.0f,-2.0f,
+		-3.0f,-1.0f, 0.0f,
+		-3.0f, 1.0f, 0.0f
+	};
+	static const GLfloat g_color_triangle1_buffer_data[] = { 
 		0.583f,  0.771f,  0.014f,
 		0.609f,  0.115f,  0.436f,
 		0.327f,  0.483f,  0.844f,
-		0.822f,  0.569f,  0.201f,
-		0.435f,  0.602f,  0.223f,
-		0.310f,  0.747f,  0.185f,
-		0.597f,  0.770f,  0.761f,
-		0.559f,  0.436f,  0.730f,
-		0.359f,  0.583f,  0.152f,
-		0.483f,  0.596f,  0.789f,
-		0.559f,  0.861f,  0.639f,
-		0.195f,  0.548f,  0.859f,
-		0.014f,  0.184f,  0.576f,
-		0.771f,  0.328f,  0.970f,
-		0.406f,  0.615f,  0.116f,
-		0.676f,  0.977f,  0.133f,
-		0.971f,  0.572f,  0.833f,
-		0.140f,  0.616f,  0.489f,
-		0.997f,  0.513f,  0.064f,
-		0.945f,  0.719f,  0.592f,
-		0.543f,  0.021f,  0.978f,
-		0.279f,  0.317f,  0.505f,
-		0.167f,  0.620f,  0.077f,
-		0.347f,  0.857f,  0.137f,
-		0.055f,  0.953f,  0.042f,
-		0.714f,  0.505f,  0.345f,
-		0.783f,  0.290f,  0.734f,
-		0.722f,  0.645f,  0.174f,
-		0.302f,  0.455f,  0.848f,
-		0.225f,  0.587f,  0.040f,
-		0.517f,  0.713f,  0.338f,
-		0.053f,  0.959f,  0.120f,
-		0.393f,  0.621f,  0.362f,
-		0.673f,  0.211f,  0.457f,
-		0.820f,  0.883f,  0.371f,
-		0.982f,  2.099f,  0.879f
+		0.583f,  0.771f,  0.014f,
+		0.609f,  0.115f,  0.436f,
+		0.327f,  0.483f,  0.844f
 	};
-
 	// Fill vertex buffer
-	GLuint vertexbuffer;
-	glGenBuffers(1, &vertexbuffer);
-	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
-
+	GLuint vertexTriangle1Buffer;
+	glGenBuffers(1, &vertexTriangle1Buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexTriangle1Buffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_triangle1_buffer_data), g_vertex_triangle1_buffer_data, GL_STATIC_DRAW);
 	// Fill color buffer
-	GLuint colorbuffer;
-	glGenBuffers(1, &colorbuffer);
-	glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), g_color_buffer_data, GL_STATIC_DRAW);
+	GLuint colorTriangle1Buffer;
+	glGenBuffers(1, &colorTriangle1Buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, colorTriangle1Buffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_triangle1_buffer_data), g_color_triangle1_buffer_data, GL_STATIC_DRAW);
+
+	/*// Triangle2
+	static const GLfloat g_vertex_triangle2_buffer_data[] = { 
+		-5.0f,-1.0f,-1.0f,
+		-5.0f,-1.0f, 1.0f,
+		-5.0f, 1.0f, 1.0f
+	};
+	static const GLfloat g_color_triangle2_buffer_data[] = { 
+		0.583f,  0.771f,  0.014f,
+		0.609f,  0.115f,  0.436f,
+		0.327f,  0.483f,  0.844f
+	};
+	// Fill vertex buffer
+	GLuint vertexTriangle2Buffer;
+	glGenBuffers(1, &vertexTriangle2Buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexTriangle2Buffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_triangle2_buffer_data), g_vertex_triangle2_buffer_data, GL_STATIC_DRAW);
+	// Fill color buffer
+	GLuint colorTriangle2Buffer;
+	glGenBuffers(1, &colorTriangle2Buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, colorTriangle2Buffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_triangle2_buffer_data), g_color_triangle2_buffer_data, GL_STATIC_DRAW);
+	*/
 
 	do{
 		// Clear screen
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		// Use our shader
-		glUseProgram(programID);
-		// Send our transformation
-		glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
-
-		// Enable buffer with attribute - coordinates
+		// Enable buffers with attributes
 		glEnableVertexAttribArray(0);
-		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-		// ID: 0, size: 3, normalised: false
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
-
-		// Enable buffer with attribute - colors
 		glEnableVertexAttribArray(1);
-		glBindBuffer(GL_ARRAY_BUFFER, colorbuffer);
-		// ID: 1, size: 3, normalised: false
+
+		// Draw Triangle1
+		// Use shader
+		glUseProgram(programTriangle1ID);
+		glUniformMatrix4fv(MatrixTriangle1ID, 1, GL_FALSE, &MVP[0][0]);
+		// Coordinates: ID: 0, size: 3, normalised: false
+		glBindBuffer(GL_ARRAY_BUFFER, vertexTriangle1Buffer);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+		// Colors: ID: 1, size: 3, normalised: false
+		glBindBuffer(GL_ARRAY_BUFFER, colorTriangle1Buffer);
 		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+		// Draw triangles
+		glDrawArrays(GL_TRIANGLES, 0, 2 * 3);
 
-		// Draw the triangle !
-		glDrawArrays(GL_TRIANGLES, 0, 12 * 3);
+		/*// Draw Triangle2
+		// Use shader
+		glUseProgram(programTriangle2ID);
+		glUniformMatrix4fv(MatrixTriangle2ID, 1, GL_FALSE, &MVP[0][0]);
+		// Coordinates: ID: 0, size: 3, normalised: false
+		glBindBuffer(GL_ARRAY_BUFFER, vertexTriangle2Buffer);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+		// Colors: ID: 1, size: 3, normalised: false
+		glBindBuffer(GL_ARRAY_BUFFER, colorTriangle2Buffer);
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+		// Draw triangles
+		glDrawArrays(GL_TRIANGLES, 0, 1 * 3);*/
 
+		// Disable buffers with attributes
 		glDisableVertexAttribArray(0);
 		glDisableVertexAttribArray(1);
 
@@ -210,9 +188,12 @@ int main(int argc, char* argv[])
 		   glfwWindowShouldClose(window) == 0 );
 
 	// Cleanup
-	glDeleteBuffers(1, &vertexbuffer);
-	glDeleteBuffers(1, &colorbuffer);
-	glDeleteProgram(programID);
+	glDeleteBuffers(1, &vertexTriangle1Buffer);
+	glDeleteBuffers(1, &colorTriangle1Buffer);
+	glDeleteProgram(programTriangle1ID);
+	/*glDeleteBuffers(1, &vertexTriangle2Buffer);
+	glDeleteBuffers(1, &colorTriangle2Buffer);
+	glDeleteProgram(programTriangle2ID);*/
 	glDeleteVertexArrays(1, &VertexArrayID);
 
 	// Close OpenGL window and terminate GLFW
